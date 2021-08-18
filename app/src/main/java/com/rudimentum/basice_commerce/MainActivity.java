ea8acc1e0d61b1a193e058bf29b05a8649c3cfc8
Victoria Grinberg <31312528+rudimentum@users.noreply.github.com>
2021-08-18 10:38:22 +0300

    Add product categories
    Use RecyclerView, create model and adapter

--- app/src/main/java/com/rudimentum/basice_commerce/MainActivity.java
+++ app/src/main/java/com/rudimentum/basice_commerce/MainActivity.java
@@ -1,14 +1,42 @@
 package com.rudimentum.basice_commerce;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.rudimentum.basice_commerce.adapter.CategoryAdapter;
+import com.rudimentum.basice_commerce.model.Category;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    RecyclerView categoryRecycler;
+    CategoryAdapter categoryAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Category> categoryList = new ArrayList<>();
+        categoryList.add(new Category(1, "Games"));
+        categoryList.add(new Category(2, "Web"));
+        categoryList.add(new Category(3, "Mobile"));
+        categoryList.add(new Category(4, "Others"));
+
+        setCategoryRecycler(categoryList);
+    }
+
+    private void setCategoryRecycler(List<Category> categoryList) {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+
+        categoryRecycler = findViewById(R.id.categoryRecycler);
+        categoryRecycler.setLayoutManager(layoutManager);
+
+        categoryAdapter = new CategoryAdapter(this, categoryList);
+        categoryRecycler.setAdapter(categoryAdapter);
     }
 }
---
+++ app/src/main/java/com/rudimentum/basice_commerce/adapter/CategoryAdapter.java
@@ -0,0 +1,52 @@
+package com.rudimentum.basice_commerce.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.rudimentum.basice_commerce.R;
+import com.rudimentum.basice_commerce.model.Category;
+
+import java.util.List;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder> {
+    Context context;
+    List<Category> categories;
+
+    public CategoryAdapter(Context context, List<Category> categories) {
+        this.context = context;
+        this.categories = categories;
+    }
+
+    @NonNull
+    @Override
+    public CategoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View categoryItems = LayoutInflater.from(context).inflate(R.layout.category_item, parent, false);
+        return new CategoryViewHolder(categoryItems);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryAdapter.CategoryViewHolder holder, int position) {
+        holder.categoryName.setText(categories.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return categories.size();
+    }
+
+    public static final class CategoryViewHolder extends RecyclerView.ViewHolder {
+        TextView categoryName;
+
+        public CategoryViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            categoryName = itemView.findViewById(R.id.categoryName);
+        }
+    }
+}
